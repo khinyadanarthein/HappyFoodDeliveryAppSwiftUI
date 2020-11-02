50126e3a2a3a2427cbd5e46f3ec7ca53cdf8c013
Ju Jue <khinyadanarthein6107@gmail.com>
2020-11-02 16:22:22 +0630

    funciton finished

---
+++ HappyFoodDeliveryApp/uiview/CheckoutSuccessView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutSuccessView.swift
+//  HappyFoodDeliveryApp
+//
+//  Created by Khin Yadanar Thein on 02/11/2020.
+//
+
+import SwiftUI
+
+struct CheckoutSuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutSuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutSuccessView()
+    }
+}
